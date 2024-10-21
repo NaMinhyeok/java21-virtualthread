8900828c2041478c8255df4cf362a1a8e66a51bf
NaMinhyeok <nmh9097@gmail.com>
2024-10-21 18:38:50 +0900

    chore: initial commit

---
+++ src/test/java/minhyeok/virtualthread/VirtualthreadApplicationTests.java
@@ -0,0 +1,13 @@
+package minhyeok.virtualthread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualthreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
